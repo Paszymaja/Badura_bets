c143e93a1e239b2de026c6fd2e6c3dd734101d34
Szymek <sprzepiora@hotmail.co.uk>
2020-05-11 12:41:11 +0200

    Otwarcie bets i czekanie na skończenie gry

--- main.py
+++ main.py
@@ -4,21 +4,40 @@ from riotwatcher import LolWatcher, ApiError
 
 region = 'EUN1'
 watcher = LolWatcher(os.getenv('LOL_KEY'))
-badura = watcher.summoner.by_name(region, 'Kedzar')
+badura = watcher.summoner.by_name(region, 'Ahegao Loli')
 badura_accountId = badura['id']
+wait_for_finish = False
 
-timeout = time.time() + 120
+timeout = time.time() + 90
+bets = []
+game_Id = 0
 
-while time.time() < timeout:
-    time.sleep(1)
+if not wait_for_finish:
+    while True:
+        time.sleep(5)
+        try:
+            live_game = watcher.spectator.by_summoner(region, badura_accountId)
+            print('game found')
+            millis = int(round(time.time() * 1000))
+            print(live_game)
+            game_Id = live_game['gameId']
+            if live_game['mapId'] == 11 and live_game['gameStartTime'] - millis < 78000:
+                print('bets open')
+                while time.time() < timeout:
+                    bets.append(int((input('Podaj czas w sec '))))
+                print('bets close')
+                wait_for_finish = True
+            break
+
+        except ApiError as err:
+            if err.response.status_code == 404:
+                print('game not found')
+            else:
+                raise
+else:
     try:
-        live_game = watcher.spectator.by_summoner(region, badura_accountId)
-        print('game found')
-        millis = int(round(time.time() * 1000))
-        if live_game['mapId'] == 1 and live_game['gameStartTime'] - millis < 78000:
-            print('bets open')
+        game_data = watcher.match.by_id(region, game_Id)
+
     except ApiError as err:
         if err.response.status_code == 404:
-            print('game not found')
-        else:
-            raise
+            print('game not finnished')
